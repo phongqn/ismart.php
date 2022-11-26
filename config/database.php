1a3ef2cd909b6a7922178d6b218ca1b9d4d5a68a
Nguyễn Phong <phongqn0901@gmail.com>
2022-11-26 15:53:04 +0700

    phong day code

---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * --------------------------------
+ * CẤU HÌNH DATABASE
+ * --------------------------------
+ * Trong phần này chúng ta khai báo các thông số để cấu hình
+ * Kết nối đến DB
+ * --------------------------------
+ * GIẢI THÍCH BIẾN
+ * --------------------------------
+ * hostname: Tên server
+ * username: Tên đăng nhập kết nối nối
+ * password: Mật khẩu kết nối
+ * database: Tên database kết nối
+ */
+
+$db = array(
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'ismartt',
+);
+// $db = array(
+//     'hostname' => 'us-cdbr-east-06.cleardb.net',
+//     'username' => 'bf8a9d50d3cbd5',
+//     'password' => '4466bc43',
+//     'database' => 'heroku_3439b71c35b4fc4',
+// );
